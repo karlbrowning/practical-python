1abab026ede66bbef7e094e29fa9358982449cc4
karlbrowning <karl.browning@goodville.com>
2022-06-20 12:15:55 -0400

    More lessons

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,20 @@
 # pcost.py
 #
 # Exercise 1.27
+
+# from asyncore import read
+
+from pickletools import read_float8
+
+
+total_cost = 0.0
+
+with open('C:\\Users\\browningk\\Documents\\GitHub\\practical-python\\Work\\Data\\portfolio.csv', 'rt') as read_data_file:
+    headers = next(read_data_file)
+    for line in read_data_file:
+        row = line.split(',')
+        nshares = int(row[1])
+        price = float(row[2])
+        total_cost += nshares * price
+
+print('Total cost', total_cost)
---
+++ Work/test.py
@@ -0,0 +1,12 @@
+def sumcount(n):
+    '''
+    Returns the sum of the first n integers
+    '''
+    total = 0
+    while n > 0:
+        total += n
+        n -= 1
+    return total
+
+a = sumcount(100)
+print(a)
